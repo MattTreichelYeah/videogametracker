78e21c5b673fe058ef6939898a928b0b8476f10b
Matt Treichel <stopdropandrolll@hotmail.com>
2017-02-21 14:06:26 -0500

    Directory Restructure

---
+++ dev/head.php
@@ -0,0 +1,24 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Gaming Collection</title>
+
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Oswald:400,300'>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/u/dt/jszip-2.5.0,pdfmake-0.1.18,dt-1.10.12,b-1.2.1,b-colvis-1.2.1,b-flash-1.2.1,b-html5-1.2.1,b-print-1.2.1,fh-3.1.2,r-2.1.0/datatables.min.css"/>
+<!--Mine-->
+<link rel="stylesheet" type="text/css" href="css/checkbox-toggle.css">
+<link rel="stylesheet" type="text/css" href="css/sidedrawer.css">
+<link rel="stylesheet" type="text/css" href="css/loading-icon.css">
+<link rel="stylesheet" type="text/css" href="css/master.css">
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/u/dt/jszip-2.5.0,pdfmake-0.1.18,dt-1.10.12,b-1.2.1,b-colvis-1.2.1,b-flash-1.2.1,b-html5-1.2.1,b-print-1.2.1,fh-3.1.2,r-2.1.0/datatables.min.js"></script>
+<!--Mine-->
+<script type="text/javascript" src="js/datatables-plugins.js"></script>
+<script type="text/javascript" src="js/touchwipe.js"></script>
+<script type="text/javascript" src="js/sidedrawer.js"></script>
+<script type="text/javascript" src="js/index-ajax.js"></script>
+<script type="text/javascript" src="js/game-table.js"></script>
+<script type="text/javascript" src="js/stats-table.js"></script>
