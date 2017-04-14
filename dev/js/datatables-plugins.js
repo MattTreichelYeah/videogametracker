8e7552bc33225d5a5375ecd56505bcd9020f88d1
Matt Treichel <stopdropandrolll@hotmail.com>
2017-04-13 23:04:55 -0400

    js - Minor formatting of file

--- dev/js/datatables-plugins.js
+++ dev/js/datatables-plugins.js
@@ -2,12 +2,9 @@
 // Not really sure why 'html' vs. 'string' part of search. String gives console names.
 
 $.fn.DataTable.ext.type.search.html = function (data) {
-    return ! data ?
-        '' :
-        typeof data === 'string' ?
-            data
-                .replace( /é/g, 'e' )
+    return ! data ? '' :
+        (typeof data === 'string' ?
+            data.replace( /é/g, 'e' )
                 .replace( /ü/g, 'u' )
-                .replace( /ñ/g, 'n' ) :
-            data;
+                .replace( /ñ/g, 'n' ) : data);
 };
