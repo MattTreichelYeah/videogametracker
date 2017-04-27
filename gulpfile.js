6ada14197e9b6add4cee3015b9e0b748a4b6f2e8
Matt Treichel <stopdropandrolll@hotmail.com>
2017-04-27 17:16:58 -0400

    gulp - Implementing gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', ['sass']);
 
 gulp.task('sass', function () {
-  return gulp.src('sass/**/*.scss')
+  return gulp.src('dev/sass/**/*.scss')
+  	.pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(gulp.dest('css'));
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dev/css'));
 });
  
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('dev/sass/**/*.scss', ['sass']);
 });
