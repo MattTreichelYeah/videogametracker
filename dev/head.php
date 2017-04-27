87be76b329edb4c4c28f1d4620c10912a8a0a1b9
Matt Treichel <stopdropandrolll@hotmail.com>
2017-04-27 17:16:42 -0400

    files - New favicon stuff

--- dev/head.php
+++ dev/head.php
@@ -2,13 +2,13 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<title>Video Game Collection Tracker</title>
+<title>Video Game Tracker</title>
 
 <link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Oswald:400,300'>
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/u/dt/jszip-2.5.0,pdfmake-0.1.18,dt-1.10.12,b-1.2.1,b-colvis-1.2.1,b-flash-1.2.1,b-html5-1.2.1,b-print-1.2.1,fh-3.1.2,r-2.1.0/datatables.min.css"/>
 <!--Mine-->
 <link rel="stylesheet" type="text/css" href="css/master.css">
-<link rel="icon" href="favicon.ico" />
+<link rel="icon" href="favicon.ico?v=1" />
 
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/u/dt/jszip-2.5.0,pdfmake-0.1.18,dt-1.10.12,b-1.2.1,b-colvis-1.2.1,b-flash-1.2.1,b-html5-1.2.1,b-print-1.2.1,fh-3.1.2,r-2.1.0/datatables.min.js"></script>
