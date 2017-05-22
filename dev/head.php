57f7beb353de79b688e36615a8acf9899c799827
Matt Treichel <stopdropandrolll@hotmail.com>
2017-05-22 15:50:42 -0400

    files - More favicon varieties

--- dev/head.php
+++ dev/head.php
@@ -8,7 +8,17 @@
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/u/dt/jszip-2.5.0,pdfmake-0.1.18,dt-1.10.12,b-1.2.1,b-colvis-1.2.1,b-flash-1.2.1,b-html5-1.2.1,b-print-1.2.1,fh-3.1.2,r-2.1.0/datatables.min.css"/>
 <!--Mine-->
 <link rel="stylesheet" type="text/css" href="css/master.css">
-<link rel="icon" href="favicon.ico?v=1" />
+<!--Favicons-->
+<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png?v=2">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png?v=2">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png?v=2">
+<link rel="manifest" href="/favicons/manifest.json?v=2">
+<link rel="mask-icon" href="/favicons/safari-pinned-tab.svg?v=2" color="#fd927e">
+<link rel="shortcut icon" href="/favicons/favicon.ico?v=2">
+<meta name="apple-mobile-web-app-title" content="VG Tracker">
+<meta name="application-name" content="VG Tracker">
+<meta name="msapplication-config" content="/favicons/browserconfig.xml?v=2">
+<meta name="theme-color" content="#ffffff">
 
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/u/dt/jszip-2.5.0,pdfmake-0.1.18,dt-1.10.12,b-1.2.1,b-colvis-1.2.1,b-flash-1.2.1,b-html5-1.2.1,b-print-1.2.1,fh-3.1.2,r-2.1.0/datatables.min.js"></script>
