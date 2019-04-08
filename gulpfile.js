37432d1bdd3324fbf379d040229f76737a385413
Matt Treichel <stopdropandrolll@hotmail.com>
2019-04-07 22:29:46 -0400

    Change some styles related to the mobile row expanding icon, and remove gulp
    Gulp was being super weird and throwing errors, and really I just need to do sass conversion anyway which I can do with this new package script

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
+// var gulp = require('gulp');
+// var sass = require('gulp-sass');
+// var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('default', ['sass']);
+// gulp.task('default', ['sass']);
 
-gulp.task('sass', function () {
-  return gulp.src('dev/sass/**/*.scss')
-  	//.pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    //.pipe(sourcemaps.write())
-    .pipe(gulp.dest('dev/css'));
-});
+// gulp.task('sass', function () {
+//   return gulp.src('dev/sass/**/*.scss')
+//   	//.pipe(sourcemaps.init())
+//     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+//     //.pipe(sourcemaps.write())
+//     .pipe(gulp.dest('dev/css'));
+// });
  
-gulp.task('sass:watch', function () {
-  gulp.watch('dev/sass/**/*.scss', ['sass']);
-});
+// gulp.task('sass:watch', function () {
+//   gulp.watch('dev/sass/**/*.scss', ['sass']);
+// });
